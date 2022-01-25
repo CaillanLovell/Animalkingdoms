6689fb620fc58ac38ea53ee8d8f1294cb8bee42d
Caillan Lovell <clovell@softwareinstitute.com>
2022-01-25 09:09:07 +0000

    new

--- src/test/java/com/tsi/Animalkingdom/test/AnimalsTest.java
+++ src/test/java/com/tsi/Animalkingdom/test/AnimalsTest.java
@@ -6,8 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnimalsTest {
     @Test
-    public void testAge(){
+    public void testSize(){
         Bat testBat = new Bat(4,4);
         assertEquals(4, testBat.getSize(), "The expected weight of the Bat was wrong");
+    }
+    @Test
+    public void testAge(){
+        Bat testBat = new Bat(4,4);
+        assertEquals(4, testBat.getAge(), "The expected age of the Bat was wrong");
     }
 }
